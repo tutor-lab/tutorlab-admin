6b69e64ce2a836b66af7c46361fc30b42476e4c3
ssu00 <rachel4535@naver.com>
2021-09-03 11:12:13 +0900

    ⚡️ added adminComponent

--- components/admin/adminComponent.jsx
+++ components/admin/adminComponent.jsx
@@ -1,31 +1,35 @@
 import { Admin, Resource } from "react-admin";
-import jsonServerProvider from "ra-data-json-server";
 import { createHashHistory } from "history";
 import { Provider } from "react-redux";
 import adminStore from "./adminStore";
-import { UserList } from "./users";
-const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+import { ClassL } from "./users";
+import simpleRestProvider from "ra-data-simple-rest";
+import { BrowserRouter } from "react-router-dom";
+const dataProvider = simpleRestProvider("http://3.35.255.192:8081/");
 const history = createHashHistory();
 const authProvider = () => Promise.resolve();
 
 const ReactAdmin = () => {
   return (
-    <Provider
-      store={adminStore({
-        authProvider,
-        dataProvider,
-        history,
-      })}
-    >
-      <Admin
-        authProvider={authProvider}
-        dataProvider={dataProvider}
-        history={history}
-        title="My Admin"
+    <BrowserRouter>
+      <Provider
+        store={adminStore({
+          authProvider,
+          dataProvider,
+          history,
+        })}
       >
-        <Resource name="users" list={UserList}></Resource>
-      </Admin>
-    </Provider>
+        <Admin
+          authProvider={authProvider}
+          dataProvider={dataProvider}
+          history={history}
+          title="My Admin"
+        >
+          <Resource name="tutors" list={ClassL} />
+          {/*tutors에서 제대로 받아오는지 확인용*/}
+        </Admin>
+      </Provider>
+    </BrowserRouter>
   );
 };
 
--- components/admin/users.jsx
+++ components/admin/users.jsx
@@ -1,26 +1,66 @@
 import * as React from "react";
 import { useMediaQuery } from "@material-ui/core";
 import { SimpleList, List, Datagrid, EmailField, TextField } from "react-admin";
+import axios from "axios";
+import { useState, useEffect } from "react";
+axios.defaults.baseURL = "http://3.35.255.192:8081";
+axios.interceptors.request.use(function (config) {
+  const token = localStorage.getItem("accessToken");
+  if (token) {
+    config.headers.Authorization = "Bearer " + token;
+  }
+  return config;
+});
 
-export const UserList = (props) => {
-  const isSmall = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+export const ClassL = (props) => {
+  const [response, setResponse] = useState("");
+  const [res, setRes] = useState(false);
+  const GetClass = async () => {
+    try {
+      setResponse(await axios.get("/tutors"));
+      setRes(true);
+      return response;
+    } catch (e) {
+      setRes(false);
+      return Promise.reject(e);
+    }
+  };
+  useEffect(() => {
+    GetClass();
+  }, []);
+  console.log(response);
 
   return (
-    <List title="All users" {...props}>
-      {isSmall ? (
-        <SimpleList
-          primaryText={(record) => record.name}
-          secondaryText={(record) => record.username}
-          tertiaryText={(record) => record.email}
-        />
-      ) : (
-        <Datagrid>
-          <TextField source="id" />
-          <TextField source="name" />
-          <TextField source="username" />
-          <EmailField source="email" />
-        </Datagrid>
-      )}
+    <List title="All Classes" {...props}>
+      <Datagrid>
+        <TextField source="title" />
+        <TextField source="name" />
+        <TextField source="username" />
+        <EmailField source="email" />
+      </Datagrid>
     </List>
   );
 };
+
+// export const UserList = (props) => {
+//   const isSmall = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+
+//   return (
+//     <List title="All users" {...props}>
+//       {isSmall ? (
+//         <SimpleList
+//           primaryText={(record) => record.name}
+//           secondaryText={(record) => record.username}
+//           tertiaryText={(record) => record.email}
+//         />
+//       ) : (
+//         <Datagrid>
+//           <TextField source="id" />
+//           <TextField source="name" />
+//           <TextField source="username" />
+//           <EmailField source="email" />
+//         </Datagrid>
+//       )}
+//     </List>
+//   );
+// };
