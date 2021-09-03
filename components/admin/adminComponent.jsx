e2b5c0a03648c9560bf5bb575891bc6c04d04fea
ssu00 <rachel4535@naver.com>
2021-09-03 11:49:22 +0900

    added adminComponent

--- components/admin/adminComponent.jsx
+++ components/admin/adminComponent.jsx
@@ -5,10 +5,9 @@ import adminStore from "./adminStore";
 import { ClassL } from "./users";
 import simpleRestProvider from "ra-data-simple-rest";
 import { BrowserRouter } from "react-router-dom";
-const dataProvider = simpleRestProvider("http://3.35.255.192:8081/tutors");
+export const dataProvider = simpleRestProvider("http://3.35.255.192:8081");
 const history = createHashHistory();
 const authProvider = () => Promise.resolve();
-
 const ReactAdmin = () => {
   return (
     <BrowserRouter>
@@ -25,7 +24,8 @@ const ReactAdmin = () => {
           history={history}
           title="My Admin"
         >
-          <Resource name="mylectures" list={ClassL} />
+          <Resource name="lectures" list={ClassL}></Resource>
+          {/* <Resource name="mylectures" list={ClassL} /> */}
         </Admin>
       </Provider>
     </BrowserRouter>
--- components/admin/users.jsx
+++ components/admin/users.jsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { useMediaQuery } from "@material-ui/core";
 import { SimpleList, List, Datagrid, EmailField, TextField } from "react-admin";
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { dataProvider } from "./adminComponent";
 axios.defaults.baseURL = "http://3.35.255.192:8081/tutors";
 axios.interceptors.request.use(function (config) {
   const token = localStorage.getItem("accessToken");
@@ -13,23 +13,16 @@ axios.interceptors.request.use(function (config) {
 });
 
 export const ClassL = (props) => {
-  const [response, setResponse] = useState("");
-  const [res, setRes] = useState(false);
-  const GetClass = async () => {
-    try {
-      setResponse(await axios.get("/mylectures"));
-      setRes(true);
-      return response;
-    } catch (e) {
-      setRes(false);
-      return Promise.reject(e);
-    }
-  };
-  useEffect(() => {
-    GetClass();
-  }, []);
-  console.log(response);
-
+  // dataProvider
+  //   .getList("mylectures", {
+  //     pagination: { page: 1, perPage: 5 },
+  //     sort: { field: "title", order: "ASC" },
+  //   })
+  //   .then((response) => console.log("response: " + response));
+  dataProvider.getOne("lectures", { id: 25 }).then((response) => {
+    console.log("response: " + response);
+    // alert(JSON.stringify(response));
+  });
   return (
     <List title="All Classes" {...props}>
       <SimpleList
